0e6ec22b8ec5b35a060c4dc77f87957b6eaf55b4
GiovanyRamdano <128889221+GiovanyRamdano@users.noreply.github.com>
2024-05-10 23:37:47 +0700

    ministudio

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + '/src/calculator.html');
+	res.sendFile(__dirname + '/src/ministudio.html');
 });
 
 const PORT = process.env.PORT || 8080;
